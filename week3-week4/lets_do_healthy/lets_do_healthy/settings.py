ac544e4e60941f2683fae30a81fc26d73dd9dab4
wsb0722 <wsb0722@naver.com>
2020-10-27 20:34:03 +0900

    week4 - Django Project news 추가

--- week3-week4/lets_do_healthy/lets_do_healthy/settings.py
+++ week3-week4/lets_do_healthy/lets_do_healthy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'food.apps.FoodConfig',
+    'news.apps.NewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
